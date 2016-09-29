6e6becd06905caa3715ba401411ee7874192222a
Brayden Kimball <Brayden@Rochelles-MacBook-Pro.local>
2016-09-29 09:53:16 -0600

    JQ-anchor-page-completed

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,3 +1,48 @@
 $(document).ready(function() {
 
+	$(function() {
+		$('#anchor1').click(function() {
+			$('html, body').animate({
+				scrollTop: $("#part-two").offset().top 
+			}, 2000)
+		return false;
+		})
+	})
+
+	$(function() {
+		$("#anchor2").click(function() {
+			$("html, body").animate({
+				scrollTop: $("#part-three").offset().top
+			}, 2000)
+			return false;
+		})
+	})
+
+	$(function() {
+		$('#link2').click(function() {
+			$('html, body').animate({
+				scrollTop: $("#part-two").offset().top 
+			}, 2000)
+		return false;
+		})
+	})
+
+	$(function() {
+		$("#link3").click(function() {
+			$("html, body").animate({
+				scrollTop: $("#part-three").offset().top
+			}, 2000)
+			return false;
+		})
+	})
+
+	$(function() {
+		$("#link1").click(function() {
+			$("html, body").animate({
+				scrollTop: $("#part-one").offset().top
+			}, 2000)
+			return false;
+		})
+	})
+
 });
